0e44ed78114c102918af44c8b93307f329fc6f12
Felipe Muniz <felipemunizicm2012@hotmail.com>
2018-08-08 17:37:41 -0300

    creating project

---
+++ src/SAGRES.Domain/Jurisdiction.cs
@@ -0,0 +1,17 @@
+﻿namespace SAGRES.Domain
+{
+    public class Jurisdiction
+    {
+        public int JurisdictionId { get; set; }
+        public int PJ { get; set; }
+        public int Power { get; set; }
+        public int IdTipoJurisdicionado { get; set; }
+        public int IdLocalidade { get; set; }
+        public string Nome { get; set; }
+        public string CodigoSagres { get; set; }
+        public bool Previdenciario { get; set; }
+        public string MunicipioImportacao { get; set; }
+        public int IdTramita { get; set; }
+        public bool HasCancelled { get; set; }
+    }
+}
---
+++ src/SAGRES.Domain/Work.cs
@@ -0,0 +1,22 @@
+﻿namespace SAGRES.Domain
+{
+    public class Work
+    {
+        public int WorkId { get; set; }
+        public int JurisdictionId { get; set; }
+        public string ContractNumber { get; set; }
+        public string WorkNumber { get; set; }
+        public string WorkDescription { get; set; }
+        public int LocalityId { get; set; }
+        public string LocalityDescription { get; set; }
+        public int WorkTypeId { get; set; }
+        public int TypeExecutionId { get; set; }
+        public double WorkValue { get; set; }
+        public string ContractedWorksheet { get; set; }
+        public string IncorporableProperty { get; set; }
+        public double Dimension { get; set; }
+        public string NumberProtocolTramit { get; set; }
+        public int WorkComplexId { get; set; }
+        public bool HasCancelled { get; set; } 
+    }
+}
